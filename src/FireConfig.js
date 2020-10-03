24440b52081c58f980f0ff117dd97e4e121c3388
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-10-03 08:55:47 +0800

    FireConfig.js

---
+++ src/FireConfig.js
@@ -0,0 +1,10 @@
+/**
+ * Create a new firebase project
+ * in https://console.firebase.google.com/
+ * then past your firebase config here to get started
+ * working on the project
+ */
+
+const firebaseConfig = {}
+
+export default firebaseConfig
